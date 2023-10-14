48bb444a74a245225b396094a7c7e76f33b67275
lullyferreira <ferreira.dev.go@gmail.com>
2023-10-13 23:08:13 -0300

    implementando 'try catch' para requisicoes http

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi'
 import './styles.css'
 
+import api from './services/api';
+
 function App(){
 
   const[input, setInput] = useState('')
 
-  function handleSearch(){
+  async function handleSearch(){
     
     if(input === ''){
       alert("Preencha o campo com o CEP")
       return;
     }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      console.log(response)
+    }catch{
+      alert("Erro ao buscar CEP")
+
+    }
+
   }
 
   return (
